beb7bc64e8d1efd9c28eb034670212e673da0b2a
rodpwn <95486831+rodpwn@users.noreply.github.com>
2022-01-03 09:37:28 -0300

    Create xfce4-panel-genmon-vpnip.sh

---
+++ Rice_My_Desktop/xfce4/panel/xfce4-panel-genmon-vpnip.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ip="$(ip a s tun0 2>/dev/null | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
+
+if [ "$ip" != "" ]; then
+  printf "<icon>network-vpn-symbolic</icon>"
+  printf "<txt>${ip}</txt>"
+  printf "<tool>VPN IP</tool>"
+else
+  printf "\0x0"
+fi
